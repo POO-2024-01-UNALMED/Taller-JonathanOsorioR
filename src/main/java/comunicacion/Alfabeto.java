77a908b513a34d9068624bb1df9b11a4b71ffe78
Jonathan <joosoriore@unal.edu.co>
2024-08-04 17:58:09 -0500

    1

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,51 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma {
+	
+	private String[] letras;
+	private String interpretacion;
+
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+	
+	public int cantidadLetras() {
+		return this.getLetras().length;
+	}
+	
+	@Override
+	public String interpretacion() {
+		
+		return this.interpretacion;
+	}
+
+	@Override
+	public String toString() {
+		String alfabeto = String.join(", ", this.getLetras());
+		return alfabeto;
+	}
+	
+	//GET Y SET
+
+	public String[] getLetras() {
+		return letras;
+	}
+
+	public void setLetras(String[] letras) {
+		this.letras = letras;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	
+	
+
+}
